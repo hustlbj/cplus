b281e456c3eb6e5ef5cb029cb2363ad80ca10463
hustlbj <hustlbj@qq.com>
2015-07-06 11:56:59 +0800

    add 'operator ='  and 'Singleton'

--- C++from0to1.cpp
+++ C++from0to1.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <cstring>
+//#include <mutex>  c++11
 using namespace std;
 
 void sumFromInput()
@@ -101,9 +103,143 @@ int copyFile(int argc, char const *argv[])
 	return 0;
 }
 
+/*
+ 空类的sizeof是1，
+ 只有构造函数和析构函数的类的sizeof是1，
+ 类中的虚函数占一个指针大小的空间，类中只有一个虚函数的话整个类还是只占一个指针大小的空间，而不是+1
+*/
+class Blank //sizeof() = 4 or 8
+{
+	Blank() {}
+	virtual ~Blank() {}
+};
+
+/*
+ 用另一个同类对象构造新对象，复制构造函数
+*/
+class A
+{
+private:
+	int value;
+public:
+	A(int n) { std::cout << "exec A(int n)" << std::endl; value = n; }
+	// A(A other)通过值传递传入other对象，会进行复制构造函数，又会进行复制，产生永无休止的递归调用。所以这儿编译会报错或者导致栈溢出。
+	A(const A& other) { std::cout << "exec A(const A& other)" << std::endl; value = other.value; }
+
+	void Print() { std::cout << value << std::endl; }
+};
+
+
+/*
+ T1. 重载赋值运算符operator =
+*/
+class CMyString
+{
+public:
+	CMyString(char* pData = NULL) { std::cout << "exec CMyString(char* pData)" << std::endl; m_pData = pData; }
+	//关于delete：用new分配的单个对象用delete，用new[]分配的对象数组用delete[]保证调用每个对象析构函数；基本类型用delete和delete[]都行。
+	~CMyString(void) { std::cout << "delete: " << std::endl; }
+	CMyString& operator =(const CMyString& str);
+	void Print() { std::cout << m_pData << std::endl; }
+private:
+	char* m_pData;
+};
+//传入参数设置为常量引用
+CMyString& CMyString::operator =(const CMyString& str)
+{
+	std::cout << "exec operator = " << std::endl;
+	//判断是不是同一个实例
+	/*
+	if (this == &str)
+		return *this;
+	//分配新内存之前先删除原来的
+	delete[] m_pData;
+	m_pData = NULL;
+	m_pData = new char[strlen(str.m_pData) + 1];
+	strcpy(m_pData, str.m_pData);
+	*/
+	if (this != &str)
+	{
+		//先创建一个临时变量
+		CMyString strTemp(str.m_pData);
+		char* pTemp = NULL;
+		pTemp = strTemp.m_pData;
+		//strTemp是临时变量，作用域结束后会自动调用析构函数把strTemp.m_pData所指向的内存释放掉
+		strTemp.m_pData = m_pData;
+		m_pData = pTemp;
+		std::cout << "m_pData: " << m_pData << std::endl;
+	}
+	//返回值要设置为该类型的引用*(this)
+	return *this;
+}
+
+/*
+ C++中可以用struct和class来定义类型。如果没有表明成员函数或者成员变量的访问权限级别，
+ 在struct中默认的是public，在class中默认的是private。
+ C#中struct定义的是值类型，值类型实例在栈上分配内存，class则是引用类型，引用类型的实例在堆上分配内存。
+*/
+
+
+/*
+ T2. Singleton单例模式
+  构造函数要私有，需要一个静态的实例，需要考虑多线程
+*/
+class Singleton1
+{
+private:
+	Singleton1() {}
+	//把复制构造函数和=操作符也设为私有，防止被复制
+	Singleton1(const Singleton1&);
+	Singleton1& operator =(const Singleton1&);
+	//在类内不能对static成员初始化，在类定义外初始化
+	//static std::recursive_mutex _lock;
+	static Singleton1* _instance;
+public:
+	static Singleton1* Instance();
+};
+//new返回分配空间的首地址——指针
+Singleton1* Singleton1::_instance = new Singleton1();
+//多线程情况下需要加锁
+Singleton1* Singleton1::Instance()
+{
+	if (_instance == NULL)
+	{
+		//std::lock_guard<std::recursive_mutex> locker(_lock);
+		if (_instance == NULL)
+		{
+			std::cout << "Singleton1 new.." << std::endl;
+			_instance = new Singleton1();
+		}
+	}
+	return _instance;
+}
+
+
 int main(int argc, char const *argv[])
 {
 	//sumFromInput();
-	copyFile(argc, argv);
+	//copyFile(argc, argv);
+	std::cout << sizeof(Blank) << std::endl;
+	//C++还可以这样创建对象。。。。直接赋值，调用了A(int n)
+	A a = 10;
+	a.Print();
+	//这里则是调用了A(const A& other)
+	A b = a;
+	b.Print();
+
+	char* str = "hehe";
+	CMyString c(str);
+	c.Print();
+	//默认执行构造函数
+	CMyString d;
+	//执行operator =
+	d = c;
+	d.Print();
+
+	//单例
+	Singleton1* s1 = Singleton1::Instance();
+	Singleton1* s2 = Singleton1::Instance();
+	std::cout << "Singleton1: " << (s1 == s2) << std::endl;
+
 	return 0;
 }
