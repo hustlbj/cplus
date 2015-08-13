4dda0f68bc9a5e9cdce03c550dd75f4a9e44776f
hustlbj <hustlbj@qq.com>
2015-08-13 22:00:41 +0800

    add select() test

---
+++ selectClient.c
@@ -0,0 +1,39 @@
+//:selectClient.c
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define BUF_LEN 1024
+
+int main(int argc, char const **argv)
+{
+	int connfd;
+	int message_len;
+	struct sockaddr_in remote_addr;
+	char buf[BUF_LEN];
+	memset(&remote_addr, 0, sizeof(remote_addr));
+	memset(buf, 0, BUF_LEN);
+	remote_addr.sin_family = AF_INET;
+	remote_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	remote_addr.sin_port = htons(8888);
+	connfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (connfd < 0)
+	{
+		perror("socket error!");
+		return -1;
+	}
+	if (connect(connfd, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr)) < 0)
+	{
+		perror("connect error!");
+		return -1;
+	}
+	message_len = recv(connfd, buf, BUF_LEN, 0);
+	buf[message_len] = '\0';
+	printf("Receive from server: %s\n", buf);
+	close(connfd);
+	return 0;
+}
---
+++ selectServer.c
@@ -0,0 +1,150 @@
+//:selectServer.c
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <error.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUF_LEN 1024
+#define SERV_PORT 6000
+#define FD_SIZE 100
+#define MAX_BACK 100
+
+int main(int argc, char **argv)
+{
+	int listenfd, connfd, sockfd, maxfd, maxi, i;
+	int nready, client[FD_SIZE]; //接收select返回值，保存客户端套接字
+	int lens;
+	size_t n; //read字节数
+	fd_set rset, allset;
+	char buf[BUF_LEN];
+	socklen_t clilen;
+	struct sockaddr_in servaddr, chiaddr;
+
+	if ((listenfd = sock(AF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		printf("Create socket Error: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1)
+	{
+		printf("Bind Error: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(listenfd, MAX_BACK) == -1)
+	{
+		printf("Listen Error: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	maxfd = listenfd;
+	maxi = -1;
+	for(i = 0; i < FD_SIZE; i ++)
+	{
+		client[i] = -1;
+	}
+	FD_ZERO(&allset);
+	FD_SET(listenfd, &allset);
+
+	while(1)
+	{
+		rset = allset;
+		//select(最大句柄数+1, 监视的文件句柄集合, 写, 异常, 超时)
+		if ((nready = select(maxfd + 1, &rset, NULL, NULL, NULL)) == -1)
+		{
+			printf("Select Error: %d\n", errno);
+			exit(EXIT_FAILURE);
+		}
+		if (nready <= 0)
+		{
+			continue;
+		}
+
+		if(FD_ISSET(listenfd, &rset))
+		{
+			clilen = sizeof(cliaddr);
+			printf("Start doing...\n");
+			//新连接
+			if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen)) == -1)
+			{
+				printf("Accept Error: %d\n", errno);
+				continue;
+			}
+			for (i = 0; i < FD_SIZE; i ++)
+			{
+				//保存这个新的client fd
+				if (client[i] < 0)
+				{
+					client[i] = connfd;
+					break;
+				}
+			}
+			if (i == FD_SIZE)
+			{
+				printf("To many clients...Reject\n");
+				close(connfd);
+				continue;
+			}
+			FD_SET(connfd, &allset);
+			if (connfd > maxfd)
+			{
+				maxfd = connfd;
+			}
+			if (i > maxi)
+			{
+				maxi = i;
+			}
+		}
+
+		for (i = 0; i <= maxi; i ++)
+		{
+			if ((sockfd = client[i]) > 0)
+			{
+				if (FD_ISSET(sockfd, &rset))
+				{
+					memset(buf, 0, sizeof(buf));
+					n = read(sockfd, buf, BUF_LEN);
+					if (n < 0)
+					{
+						printf("Error!\n");
+						close(sockfd);
+						FD_CLR(sockfd, &allset);
+						client[i] = -1;
+						continue;
+					}
+					if (n == 0)
+					{
+						printf("No data\n");
+						close(sockfd);
+						FD_CLR(sockfd, &allset);
+						client[i] = -1;
+						continue;
+					}
+
+					printf("Server Recv: %s\n", buf);
+					if (strcmp(buf, "q") == 0)
+					{
+						close(sockfd);
+						FD_CLR(sockfd, &allset);
+						client[i] = -1;
+						continue;
+					}
+					printf("Server send: %s\n", buf);
+					write(sockfd, buf, n);
+				}
+			}
+		}
+	}
+	return 0;
+}
